1551ebbe0bfc23e206e4d79537a80492dc3c12be
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2018-11-13 22:39:52 +0800

    ➖ remove prerender-spa-plugin

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,9 +2,9 @@
 
 const path = require('path')
 const fs = require('fs')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
 const SizePlugin = require('size-plugin')
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
+// const PrerenderSPAPlugin = require('prerender-spa-plugin')
+// const HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
 
 const isProductionEnvFlag = process.env.NODE_ENV === 'production'
 
@@ -127,12 +127,14 @@ module.exports = {
 
   configureWebpack: {
     plugins: [
+      /*
       isProductionEnvFlag ? new PrerenderSPAPlugin({
         // Required - The path to the webpack-outputted app to prerender.
         staticDir: path.join(__dirname, 'dist'),
         // Required - Routes to render.
         routes: ['/', '/explore']
       }) : () => {},
+      */
       // NEED FIX 🚧 : HardSourceWebpackPlugin Will Cause Error.
       // new HardSourceWebpackPlugin(),
       isProductionEnvFlag ? new SizePlugin() : () => {}
