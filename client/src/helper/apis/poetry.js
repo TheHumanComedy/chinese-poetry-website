5765d8e7ab69e9dcf167dd97b8a8d97aae0b5fcb
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2018-11-25 17:10:10 +0800

    :wrench: update request base url

--- client/src/helper/apis/poetry.js
+++ client/src/helper/apis/poetry.js
@@ -5,7 +5,7 @@ import Vue from 'vue'
 
 /* @desc: 统一组装 HTTP 请求的 Url 地址 */
 function composeApiPath(api) {
-  const BASE_URL = Vue.config.isProduction ? 'http://39.108.93.82:8888' : ''
+  const BASE_URL = Vue.config.isProduction ? 'http://poem.lovejade.cn:8888' : ''
   return `${BASE_URL}/api/${api}`
 }
 
